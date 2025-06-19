0d040b9aeb3ac4450349c34c5d0cb437d01ebe17
Lief Koepsel <lkoepsel@wellys.com>
2025-06-19 09:20:56 -0700

    fixed blink_wo_delay to indicate when a rollover has occured

--- examples/blink_wo_delay/main.c
+++ examples/blink_wo_delay/main.c
@@ -12,41 +12,42 @@
 #define WHITE 3         // white LED to pin 3
 #define RED 4           // red LED to pin 4
 
-volatile uint16_t total_YELLOW = 0;
-volatile uint16_t total_RED = 0;
+volatile uint16_t total_blinks = 0;
+volatile uint16_t total_rollover = 0;
 
 int main (void)
 {
   // Variables will change:
   uint16_t previous_ticks = 0;        // will store last time LED was updated
+  uint16_t last_tick_check = 0;        // will store last time LED was updated
+
   // Set up a system tick of 1 millisec (1kHz)
   init_sysclock_1k ();
 
     /* set pin to output*/
-    DDRB |= (_BV(YELLOW) | _BV(WHITE) | _BV(RED));
-    PORTB |= (_BV(RED));
-    PORTB |= (_BV(WHITE));
+    DDRB |= (_BV(YELLOW));
 
     while(1)
     {
+        uint16_t current_ticks = ticks();
+
         // check to see if it's time to blink the LED; that is, if the 
         // difference between the current time and last time you blinked 
         // the LED is bigger than the interval at which you want to 
         // blink the LED.
-        uint16_t current_ticks = ticks();
-        if (current_ticks < previous_ticks)
-        {
-            total_RED++;
-            asm ("sbi %0, %1 \n" : : "I" (_SFR_IO_ADDR(PINB)), "I" (RED));
-        }
-        if(current_ticks - previous_ticks > interval ) 
+        if(current_ticks - previous_ticks >= interval ) 
         {
             // save the last time you blinked the LED 
             previous_ticks = current_ticks;   
+            total_blinks++;
             // toggle the state of the LED
-            // PINB |= (_BV(YELLOW));
-            total_YELLOW++;
             asm ("sbi %0, %1 \n" : : "I" (_SFR_IO_ADDR(PINB)), "I" (YELLOW));
         }
+
+        if (current_ticks < last_tick_check)
+        {
+            total_rollover++;
+        }
+        last_tick_check = current_ticks;   
     }
 }

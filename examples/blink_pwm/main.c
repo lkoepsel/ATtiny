9a56af2876b38d83de7184a7e0b805b0bb9d0032
lief koepsel <lkoepsel@wellys.com>
2025-06-22 09:49:51 -0700

    blink_pwm, kind of works

--- examples/blink_pwm/main.c
+++ examples/blink_pwm/main.c
@@ -4,61 +4,57 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-#define DELAY 5
+#define DELAY 10
 
 volatile uint8_t brightnessA;
 volatile uint8_t brightnessB;
+volatile uint8_t TOP = 255;
 
 // -------- Functions --------- //
 static inline void initTimer0(void) 
 {
-    TCCR0A = 0 ;                // Normal operation 
-    TCCR0B |= ( _BV(CS01) | _BV(CS00)) ; // /64
+    TCCR0A = 0 ;                                        // Normal operation 
+    TCCR0B |= ( _BV(CS01) | _BV(CS00)) ;                // /64
     TIMSK0 |= (_BV(OCIE0B) | _BV(OCIE0A) | _BV(TOIE0)); // turn on all interrupts
     sei();
 }
 
 ISR(TIM0_OVF_vect) 
 {
-    PORTB |= (_BV(PORTB1) | _BV(PORTB0));
+    PORTB |= (_BV(PORTB1));
     OCR0A = brightnessA;
     OCR0B = brightnessB;
 }
 ISR(TIM0_COMPA_vect) 
 {
-    PORTB &= ~(_BV(PORTB0));
     PORTB |= (_BV(PORTB1));
 }
 ISR(TIM0_COMPB_vect) 
 {
-    PORTB |= (_BV(PORTB0));
     PORTB &= ~(_BV(PORTB1));
 }
 
 int main(void) {
 
     uint8_t i;
-    DDRB |= (_BV(PORTB0) | _BV(PORTB1));
+    DDRB |= (_BV(PORTB1));
     initTimer0();
 
-    while (1) {
-
-    // loop to fade bright (fade on)
-    for (i = 0; i < 255; i++) 
+    while (1) 
     {
-        _delay_ms(DELAY);
-        brightnessA = i;
-        brightnessB = 255 - i;
-    }
-    _delay_ms(1000);
+        for (i = 0; i < TOP; i++) 
+        {
+            _delay_ms(DELAY);
+            brightnessA = i;
+        }
+        _delay_ms(500);
 
-    // loop to fade dim (fade off)
-    for (i = 254; i > 0; i--) {
-      _delay_ms(DELAY);
-      brightnessA = i;
-      brightnessB = 255 - i;
-    }
-    _delay_ms(1000);
+        for (i = TOP; i > 0; i--) {
+          _delay_ms(DELAY);
+          brightnessA = i;
+          // brightnessB = BOT - i;
+        }
+        _delay_ms(500);
 
   }                                                  // End event loop 
   return 0;                            // This line is never reached 

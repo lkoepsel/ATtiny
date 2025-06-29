acc312ec021df44ce5eff5a4a8daa32204af2e89
Lief Koepsel <lkoepsel@wellys.com>
2025-06-29 09:53:01 -0700

    Cleaned up blink_pwm, works and optimized

--- examples/blink_pwm/main.c
+++ examples/blink_pwm/main.c
@@ -1,5 +1,7 @@
-// Quick and dirty demo of how to get PWM on any pin with interrupts
-// ------- Preamble -------- //
+// blink demo of how to get PWM on any pin with interrupts
+// based on code by Mike Williams, Make AVR book
+// uses asm commmands where possible to save memory
+
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
@@ -10,15 +12,6 @@
 
 volatile uint8_t A_pulse = 32;
 volatile uint8_t B_pulse = 63;
-
-// -------- Functions --------- //
-static inline void initTimer0(void) 
-{
-    TCCR0A = 0 ;                                        // Normal operation 
-    TCCR0B |= ( _BV(CS01) | _BV(CS00)) ;                // /64
-    TIMSK0 |= (_BV(OCIE0B) | _BV(OCIE0A) | _BV(TOIE0)); // turn on all interrupts
-    sei();
-}
 
 ISR(TIM0_OVF_vect) 
 {
@@ -36,12 +29,18 @@ ISR(TIM0_COMPB_vect)
     asm ("cbi %0, %1 \n" : : "I" (_SFR_IO_ADDR(PORTB)), "I" (YELLOW));
 }
 
-int main(void) {
+int main(void) 
+{
+    // setup clock for hybrid PWM operation
+    TCCR0A = 0 ;                                        // Normal operation 
+    TCCR0B |= ( _BV(CS01) | _BV(CS00)) ;                // /64 prescalar (72Hz)
+    TIMSK0 |= (_BV(OCIE0B) | _BV(OCIE0A) | _BV(TOIE0)); // turn on all interrupts
+    sei();
 
-    DDRB |= (_BV(YELLOW) | _BV(GREEN));
-    initTimer0();
+    asm ("sbi %0, %1 \n" : : "I" (_SFR_IO_ADDR(DDRB)), "I" (GREEN));
+    asm ("sbi %0, %1 \n" : : "I" (_SFR_IO_ADDR(DDRB)), "I" (YELLOW));
 
-    while (1) 
+    while (1)  // loop can be used to adjust duty cycle of two pwm signals
     {
         _NOP();
     }                                         

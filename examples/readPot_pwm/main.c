bf9fb7b674b874df7e3636e2fe51a6aede7d2703
Lief Koepsel <lkoepsel@wellys.com>
2025-06-30 10:15:08 -0700

    readPot_pwm works for three levels of intensity

--- examples/readPot_pwm/main.c
+++ examples/readPot_pwm/main.c
@@ -4,16 +4,37 @@
 #include <util/atomic.h>
 
 
-#define GREEN PB0
-#define YELLOW PB1
-#define BLUE PB2
+#define BLUE PB0
+#define GREEN PB1
+#define WHITE PB2
 #define POT PB4
 #define TOP 642
 #define MID 341
+#define HIGH 250
+#define MOD 127
+#define DIM 32
+#define set_PIN(x) (1 << (x))
+#define clr_PIN(x) (~(1 << (x)))
 
 volatile uint16_t ADC_result = 0;
+volatile uint8_t LED_intensity = 63;
+volatile uint8_t LED_pin = 0;
+
 
 // -------- Functions --------- //
+// ISR to set the pin high and the PWM frequency
+ISR(TIM0_OVF_vect) 
+{
+    OCR0A = LED_intensity;
+    PORTB |= set_PIN(LED_pin);
+}
+
+// ISR to set the pin low, thus the duty cycle of the A_pulse
+ISR(TIM0_COMPA_vect) 
+{
+    PORTB &= clr_PIN(LED_pin);
+}
+
 ISR (ADC_vect)      
 {
     ADC_result = ADC;                  // read ADC value
@@ -27,7 +48,7 @@ uint16_t read_ADC(void) {
     return 0;   
 }
 
-static inline void initADC0(void) 
+static inline void initADC(void) 
 {
     ADMUX = 0;  // Clear ADMUX first
     ADMUX |= _BV(MUX1);  // Select ADC2 (PB4), REFS0 = 0, VCC as reference
@@ -38,44 +59,91 @@ static inline void initADC0(void)
     sei();
 }
 
+static inline void initTC(void) 
+{
+    // setup clock for hybrid PWM operation
+    TCCR0A = 0 ;                                        // Normal operation 
+    TCCR0B |= ( _BV(CS00) ) ;                // /8 prescalar (72Hz)
+    TIMSK0 |= ( _BV(OCIE0A) | _BV(TOIE0)); // turn on interrupts
+    sei();
+}
+
 int main(void)
 {
-    initADC0();
+    initADC();
+    initTC();
 
     /* set pins to output */
-    DDRB |=( _BV(GREEN) | _BV(YELLOW) | _BV(BLUE)); 
-    // Make sure PB2 is an input (it should be by default)
+    DDRB |=( _BV(BLUE) | _BV(GREEN) | _BV(WHITE)); 
+    // Make sure PB4 is an input (it should be by default)
     DDRB &= ~_BV(POT);
-    PORTB |=( _BV(GREEN) | _BV(YELLOW) | _BV(BLUE));  // set all high
+    PORTB |=( _BV(BLUE) | _BV(GREEN) | _BV(WHITE));  // set all high
     _delay_ms(500);
 
-    PORTB &= ~( _BV(GREEN) | _BV(YELLOW) | _BV(BLUE));  // set all high
-    ADCSRA |= _BV(ADSC);                    // start ADC conversion
+    PORTB &= ~( _BV(BLUE) | _BV(GREEN) | _BV(WHITE));  // set all low
+    ADCSRA |= _BV(ADSC);                    // begin ADC conversions
 
-    volatile uint16_t curr_result = read_ADC();
-    volatile uint16_t prev_result = curr_result;
     for (;;)
     {
-        curr_result = read_ADC();
-        prev_result = curr_result;
+        volatile uint16_t curr_result = read_ADC();
+        volatile uint16_t prev_result = curr_result;
 
         while (prev_result == curr_result)
         {
             curr_result = read_ADC();
             if (curr_result > TOP) 
             {
-                PORTB &= ~_BV(YELLOW) & ~_BV(BLUE);
-                PORTB |= _BV(GREEN);  
+                // 642 - 1023 ADC
+                PORTB &= ~_BV(GREEN) & ~_BV(WHITE);
+                LED_pin = BLUE;
+                if (curr_result > (TOP + 220))
+                {
+                    LED_intensity = HIGH;  
+                }
+                else if (curr_result > (TOP + 110))
+                {
+                    LED_intensity = MOD;  
+                }
+                else
+                {
+                    LED_intensity = DIM;
+                }
             }
             else if (curr_result > MID)
             {
-                PORTB |= _BV(YELLOW);
-                PORTB &= ~_BV(GREEN) & ~_BV(BLUE);
+                // 341 - 641 ADC
+                PORTB &= ~_BV(BLUE) & ~_BV(WHITE);
+                LED_pin = GREEN;  
+                if (curr_result > (MID + 220))
+                {
+                    LED_intensity = HIGH;  
+                }
+                else if (curr_result > (MID + 110))
+                {
+                    LED_intensity = MOD;  
+                }
+                else
+                {
+                    LED_intensity = DIM;
+                }
             }
             else
             {
-                PORTB |= _BV(BLUE);
-                PORTB &= ~_BV(YELLOW) & ~_BV(GREEN);
+                // 0 - 340 ADC
+                PORTB &= ~_BV(GREEN) & ~_BV(BLUE);
+                LED_pin = WHITE;  
+                if (curr_result > (220))
+                {
+                    LED_intensity = HIGH;  
+                }
+                else if (curr_result > (110))
+                {
+                    LED_intensity = MOD;  
+                }
+                else
+                {
+                    LED_intensity = DIM;
+                }
             }
         }
     }

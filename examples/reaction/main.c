7749e29a53a9e6e32eb56a764f650e70186e16fb
Lief Koepsel <lkoepsel@wellys.com>
2025-09-20 13:45:57 -0700

    added celebrate to test blinking routine, added it to reaction, now to test

--- examples/reaction/main.c
+++ examples/reaction/main.c
@@ -1,5 +1,5 @@
 // reaction - simple reaction time game
-// FLASH Hash - 9b945b4682713a764ce2a82d14e9f8e4
+// FLASH Hash - c73ff08c657fe5e16a8a438eda6767b9
 // Game Rules
 // 1. Light BLUE between .5 and 2 seconds
 // 2. Blink GREEN to start.
@@ -34,39 +34,42 @@
 #define BUTTON PB3
 #define TOLERANCE 4
 
+// DEBUG: This pin will toggle if enabled using COM0A0
+#define OC0A_PIN PB0       // (OC0A for ATtiny13A)
+
 volatile uint8_t ticks_ctr = 0;
 
 // Incrments ticks_ctr for measuring time
 ISR (TIM0_COMPA_vect)      
 {
     ticks_ctr++;
-
-    // DEBUG: Use to check frequency, currently 200Hz (1/(25ms * 2))
-    // SBI(PINB, PB3);
 }
 
-// Initialize timer to ~40 ticks for 1 seconds (1 tick = 25ms or max = ~6.5)
+// Initialize timer to ~40 ticks for 1 seconds (1 tick = 25.08ms)
 // Initialize timer 0 to CTC Mode using OCR0A, with a chip clock of 1.2Mhz
-// The values below will result in a ~62.5Hz counter (40 ticks = 1 second)
+// The values below will result in a ~20Hz counter (2 ticks = 1 period or ~50ms, 40 ticks = 1 second)
 // CTC mode (WGM0[2:0] = 2)
 // Set clock select to /256 CS => 100
 // Bit 2 – OCIE0A: Timer/Counter0 Output Compare Match A Interrupt Enable
-// OCR0A = x75
+// OCR0A = x70
 // TC0 Register Values, as working
-// "GTCCR", 0x00000048, 0x00 (0, 0b00000000), PSR10: 0b0, TSM: 0b0
-// "OCR0B", 0x00000049, 0x00 (0, 0b00000000)
-// "TCCR0A", 0x0000004F, 0x02 (2, 0b00000010), WGM0: 0b10, COM0B: 0b00, COM0A: 0b00
-// "TCNT0", 0x00000052, 0x4C (76, 0b01001100)
-// "TCCR0B", 0x00000053, 0x04 (4, 0b00000100), CS0: 0b100, WGM02: 0b0, FOC0B: 0b0, FOC0A: 0b0
-// "OCR0A", 0x00000056, 0x75 (117, 0b01110101)
-// "TIFR0", 0x00000058, 0x08 (8, 0b00001000), TOV0: 0b0, OCF0A: 0b0, OCF0B: 0b1
-// "TIMSK0", 0x00000059, 0x04 (4, 0b00000100), TOIE0: 0b0, OCIE0A: 0b1, OCIE0B: 0b0
-void init_sysclock (void)          
+// `tc0`, `gtccr`, "GTCCR", 0x00000048, 8-bit | 0x00 (0, 0b00000000), PSR10: 0b0, TSM: 0b0
+// `tc0`, `ocr0b`, "OCR0B", 0x00000049, 8-bit | 0x00 (0, 0b00000000)
+// `tc0`, `tccr0a`, "TCCR0A", 0x0000004F, 8-bit | 0x42 (66, 0b01000010), WGM0: 0b10, COM0B: 0b00, COM0A: 0b01
+// `tc0`, `tcnt0`, "TCNT0", 0x00000052, 8-bit | 0x4F (79, 0b01001111)
+// `tc0`, `tccr0b`, "TCCR0B", 0x00000053, 8-bit | 0x04 (4, 0b00000100), CS0: 0b100, WGM02: 0b0, FOC0B: 0b0, FOC0A: 0b0
+// `tc0`, `ocr0a`, "OCR0A", 0x00000056, 8-bit | 0x70 (112, 0b01110000)
+// `tc0`, `tifr0`, "TIFR0", 0x00000058, 8-bit | 0x08 (8, 0b00001000), TOV0: 0b0, OCF0A: 0b0, OCF0B: 0b1
+// `tc0`, `timsk0`, "TIMSK0", 0x00000059, 8-bit | 0x04 (4, 0b00000100), TOIE0: 0b0, OCIE0A: 0b1, OCIE0B: 0b0
+
+void init_20Hz (void)          
 {
-    TCCR0A = ( _BV(WGM01) ) ; 
+    TCCR0A = _BV(WGM01) ; 
+    // DEBUG: show frequency on PB0
+    // TCCR0A |= _BV(COM0A0) ; 
     TCCR0B |= ( _BV(CS02) ) ;
     TIMSK0 |= _BV(OCIE0A);
-    OCR0A = 0x75;
+    OCR0A = 0x70;
     sei();
  }
 
@@ -107,7 +110,7 @@ void init_sysclock (void)
     return button_end;
 }
 
-void blink(uint8_t n)
+void blink(uint8_t b)
 {
     do
     {
@@ -120,7 +123,7 @@ void blink(uint8_t n)
         SBI(PORTB, GREEN);
         _delay_ms(125);
         CBI(PORTB, GREEN);
-    } while(--n);     
+    } while(--b);     
 }
 
 // main - infinite loop, doesn't exit
@@ -129,10 +132,10 @@ int main (void)
     for (;;)
     {
     // Initialize timer for a max of ~6.5s (1 tick = 25ms)
-        init_sysclock ();
+        init_20Hz ();
 
         // DEBUG: temp pin for determing freq
-        // DDRB |= (_BV(PB3));
+        // DDRB |= (_BV(OC0A_PIN));
 
         /* setup LEDs */
         DDRB |=( _BV(BLUE) | _BV(GREEN) | _BV(RED));
@@ -215,13 +218,8 @@ int main (void)
         
         } while (--try);
 
-        if (good > 0)
-        {
-            do
-            {
-                blink(good * 2);
-            } while ( --good);
-        }
+        volatile uint8_t blinks = 1 << good;
+        blink(blinks);
         _delay_ms(1000);
     }
 }

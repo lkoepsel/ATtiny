37a71ddf9e7d25e9f486026e8bfaec9db3a47dc4
Lief Koepsel <lkoepsel@wellys.com>
2025-11-23 16:12:12 -0800

    added ATtiny.h to blink_macro and ticks_read

---
+++ examples/ticks_read/ATtiny.h
@@ -0,0 +1,31 @@
+// ATtinylib: Library of macros and defines for ATtiny13A
+
+#ifndef ATTINYLIB_H
+#define ATTINYLIB_H
+
+#include <stdlib.h>
+#include <avr/io.h>
+
+  #define SBI(port, bit) \
+    __asm__ __volatile__ ( \
+        "sbi %0, %1" \
+        : \
+        : "I" (_SFR_IO_ADDR(port)), \
+          "I" (bit) \
+    )
+
+    #define CBI(port, bit) \
+    __asm__ __volatile__ ( \
+        "cbi %0, %1" \
+        : \
+        : "I" (_SFR_IO_ADDR(port)), \
+          "I" (bit) \
+    )
+
+  #define TIMER_DELAY(ticks) do { \
+      TCNT0 = 0; \
+      while (TCNT0 < (ticks)) {} ; \
+  } while(0)
+
+
+    #endif
